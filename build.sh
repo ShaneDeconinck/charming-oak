e09fc371fef508b602748fe34a719205e48b2548
Shane Deconinck <shane.deconinck@howest.be>
2022-11-22 20:01:21 +0100

    wip

--- build.sh
+++ build.sh
@@ -5,8 +5,7 @@ set -o pipefail
 set -v
 
 initialGitHash=$(git rev-list --max-parents=0 HEAD)
-node ./studio-build.js $initialGitHash &
-
+# node ./studio-build.js $initialGitHash 
 npx @stackbit/sanity-pull --ssg gatsby --sanity-project-id $SANITY_PROJECT_ID --sanity-access-token $SANITY_ACCESS_TOKEN
 gatsby build
 
