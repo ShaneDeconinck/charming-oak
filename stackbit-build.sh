04425ac4d0c14336fb56d718b8aaf8b72da84649
Shane Deconinck <shane.deconinck@gmail.com>
2022-11-22 19:19:04 +0100

    Update stackbit-build.sh

--- stackbit-build.sh
+++ stackbit-build.sh
@@ -7,8 +7,8 @@ set -v
 initialGitHash=$(git rev-list --max-parents=0 HEAD)
 node ./studio-build.js $initialGitHash &
 
-npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.stackbit.com/pull/5d9314d5e322320010197cc3
+#npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.stackbit.com/pull/5d9314d5e322320010197cc3
 gatsby build
 
 wait
-echo "Stackbit-build.sh finished build"
+echo "Build finished"
